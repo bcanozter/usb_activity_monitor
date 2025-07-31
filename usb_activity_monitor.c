06d8526475a002f0392eafdeadcf9ce1103d0189
Burak Ozter <burak.ozter@spirirobotics.com>
2025-07-31 10:46:32 -0300

    WIP: singly linked list for the list of usb devices

--- usb_activity_monitor.c
+++ usb_activity_monitor.c
@@ -15,14 +15,11 @@
 
 static DEFINE_MUTEX(rw_mutex);
 static struct proc_dir_entry *proc_file;
-static struct usb_device_info *usb_devices_array;
-static int device_count = 0;
-#define MAX_USB_DEVICES 100
-#define MAX_STRING_LENGTH 255
+#define MAX_STRING_LENGTH 256
 struct usb_device_info
 {
     int bus_num;
-    int dev_num;
+    int device_num;
     u16 vendor_id;
     u16 product_id;
     char manufacturer[MAX_STRING_LENGTH];
@@ -31,101 +28,99 @@ struct usb_device_info
     bool is_connected;
     struct timespec64 added_time;
     struct timespec64 removed_time;
+    struct usb_device_info *next;
 };
 
-static int find_device_in_array(int bus_num, int dev_num)
+struct usb_device_list
 {
-    int i;
-    for (i = 0; i < device_count; i++)
-    {
-        if (usb_devices_array[i].bus_num == bus_num &&
-            usb_devices_array[i].dev_num == dev_num)
-        {
-            return i;
-        }
-    }
-    return -1;
-}
+    struct usb_device_info *head;
+    struct usb_device_info *tail;
+    int size;
+};
 
-static int add_device_to_array(struct usb_device *device)
+static struct usb_device_list usb_devices = {.head = NULL, .tail = NULL, .size = 0};
+
+// TODO add limit
+static void add_device_to_list(struct usb_device_info *new_device)
 {
-    // TODO: Either create a hard cap or circular linked list or something.
-    //  MAX_USB_DEVICES is not respected. This will cause really bad things.
-    int index = device_count; //?
-    int ret;
-    ret = mutex_trylock(&rw_mutex);
-    if (ret != 0)
+    new_device->next = NULL;
+    if (!usb_devices.head)
     {
-        usb_devices_array[index].bus_num = device->bus ? device->bus->busnum : -1;
-        usb_devices_array[index].dev_num = device->devnum;
-        usb_devices_array[index].vendor_id = le16_to_cpu(device->descriptor.idVendor);
-        usb_devices_array[index].product_id = le16_to_cpu(device->descriptor.idProduct);
-        usb_devices_array[index].is_connected = true;
-
-        if (device->manufacturer)
-        {
-            strncpy(usb_devices_array[index].manufacturer, device->manufacturer,
-                    MAX_STRING_LENGTH - 1);
-            usb_devices_array[index].manufacturer[MAX_STRING_LENGTH - 1] = '\0';
-        }
-        else
-        {
-            strcpy(usb_devices_array[index].manufacturer, "Unknown");
-        }
-
-        if (device->product)
-        {
-            strncpy(usb_devices_array[index].product, device->product,
-                    MAX_STRING_LENGTH - 1);
-            usb_devices_array[index].product[MAX_STRING_LENGTH - 1] = '\0';
-        }
-        else
-        {
-            strcpy(usb_devices_array[index].product, "Unknown");
-        }
-
-        if (device->serial)
-        {
-            strncpy(usb_devices_array[index].serial, device->serial,
-                    MAX_STRING_LENGTH - 1);
-            usb_devices_array[index].serial[MAX_STRING_LENGTH - 1] = '\0';
-        }
-        else
-        {
-            strcpy(usb_devices_array[index].serial, "Unknown");
-        }
-        ktime_get_real_ts64(&usb_devices_array[index].added_time);
-        usb_devices_array[index].removed_time.tv_sec = 0;
-        usb_devices_array[index].removed_time.tv_nsec = 0;
-        device_count++;
-        mutex_unlock(&rw_mutex);
-        return index;
+        usb_devices.head = new_device;
+        usb_devices.tail = new_device;
     }
     else
     {
-        printk(KERN_DEBUG "Failed to acquire lock.\n");
-        return -1;
+        usb_devices.tail->next = new_device;
+        usb_devices.tail = new_device;
     }
+    usb_devices.size++;
 }
 
-static void remove_device_from_array(int bus_num, int dev_num)
+static struct usb_device_info *find_device_in_list(int bus_num, int device_num)
 {
-    int ret;
-    ret = mutex_trylock(&rw_mutex);
-    if (ret != 0)
+    struct usb_device_info *device = usb_devices.head;
+    while (device != NULL)
     {
-        int index = find_device_in_array(bus_num, dev_num);
-
-        if (index >= 0)
+        if (device->bus_num == bus_num && device->device_num == device_num)
         {
-            usb_devices_array[index].is_connected = false;
-            ktime_get_real_ts64(&usb_devices_array[index].removed_time);
+            return device;
         }
-        mutex_unlock(&rw_mutex);
+        device = device->next;
+    }
+    return NULL;
+}
+
+static void add_usb_device(struct usb_device *device)
+{
+    struct usb_device_info *new_device = kmalloc(sizeof(struct usb_device_info), GFP_KERNEL);
+    if (!new_device)
+    {
+        return;
+    }
+    new_device->bus_num = device->bus ? device->bus->busnum : -1;
+    new_device->device_num = device->devnum;
+    new_device->vendor_id = le16_to_cpu(device->descriptor.idVendor);
+    new_device->product_id = le16_to_cpu(device->descriptor.idProduct);
+    new_device->is_connected = true;
+    if (device->manufacturer)
+    {
+        strncpy(new_device->manufacturer, device->manufacturer, MAX_STRING_LENGTH - 1);
     }
     else
     {
-        printk(KERN_DEBUG "Failed to acquire lock.\n");
+        strcpy(new_device->manufacturer, "Unknown");
+    }
+    if (device->product)
+    {
+        strncpy(new_device->product, device->product, MAX_STRING_LENGTH - 1);
+    }
+    else
+    {
+        strcpy(new_device->product, "Unknown");
+    }
+    if (device->serial)
+    {
+        strncpy(new_device->serial, device->serial, MAX_STRING_LENGTH - 1);
+    }
+    else
+    {
+        strcpy(new_device->serial, "Unknown");
+    }
+    ktime_get_real_ts64(&new_device->added_time);
+    new_device->removed_time.tv_sec = 0;
+    new_device->removed_time.tv_nsec = 0;
+    add_device_to_list(new_device);
+}
+
+// Mark device as disconnected and set removal time
+static void disconnect_usb_device(int bus_num, int device_num)
+{
+    struct usb_device_info *dev = find_device_in_list(bus_num, device_num);
+    if (dev && dev->is_connected)
+    {
+        dev->is_connected = false;
+        ktime_get_real_ts64(&dev->removed_time);
     }
 }
 
@@ -159,12 +154,12 @@ static int usb_notifier_callback(struct notifier_block *self,
     case USB_DEVICE_ADD:
         printk(KERN_WARNING PROC_FILENAME ": USB device has been added.\n");
         print_usb_device_info(udev);
-        add_device_to_array(udev);
+        add_usb_device(udev);
         break;
     case USB_DEVICE_REMOVE:
         printk(KERN_WARNING PROC_FILENAME ": USB device has been removed.\n");
         print_usb_device_info(udev);
-        remove_device_from_array(udev->bus ? udev->bus->busnum : -1, udev->devnum);
+        disconnect_usb_device(udev->bus ? udev->bus->busnum : -1, udev->devnum);
         break;
     }
     return NOTIFY_OK;
@@ -174,42 +169,34 @@ static struct notifier_block usb_notifier_block = {
     .notifier_call = usb_notifier_callback,
 };
 
-// TODO: Don't like this print structure. It is OK for debugging right now.
+// Show devices in /proc
 static int usb_devices_show(struct seq_file *m, void *v)
 {
-    int i;
+    struct usb_device_info *curr = usb_devices.head;
     seq_printf(m, "USB Activity Monitor - Device List\n");
     seq_printf(m, "================================\n\n");
-    seq_printf(m, "Total Number of Devices: \033[1m%d\033[0m\n\n", device_count);
-    for (i = 0; i < device_count; i++)
-    {
-        seq_printf(m, "\033[1mDevice %d:\n\033[0m", i + 1);
-        seq_printf(m, "  Bus: %03d, Device: %03d\n",
-                   usb_devices_array[i].bus_num,
-                   usb_devices_array[i].dev_num);
-        seq_printf(m, "  VID:PID: 0x%04x:0x%04x\n",
-                   usb_devices_array[i].vendor_id,
-                   usb_devices_array[i].product_id);
-        seq_printf(m, "  Manufacturer: %s\n",
-                   usb_devices_array[i].manufacturer);
-        seq_printf(m, "  Product: %s\n",
-                   usb_devices_array[i].product);
-        seq_printf(m, "  Serial: %s\n",
-                   usb_devices_array[i].serial);
-        seq_printf(m, "  Added: %lld.%09lu\n",
-                   (long long)usb_devices_array[i].added_time.tv_sec, usb_devices_array[i].added_time.tv_nsec);
-        if (!usb_devices_array[i].is_connected)
-            seq_printf(m, "  Removed: %lld.%09lu\n",
-                       (long long)usb_devices_array[i].removed_time.tv_sec, usb_devices_array[i].removed_time.tv_nsec);
-        seq_printf(m, "  Status: %s\n\n",
-                   usb_devices_array[i].is_connected ? "\033[32mConnected\033[0m" : "\033[31mDisconnected\033[0m");
-    }
-
-    if (device_count == 0)
+    seq_printf(m, "Total Number of Devices: \033[1m%d\033[0m\n\n", usb_devices.size);
+    if (!curr)
     {
         seq_printf(m, "No USB devices found.\n");
+        return 0;
     }
-
+    int i = 1;
+    while (curr)
+    {
+        seq_printf(m, "\033[1mDevice %d:\n\033[0m", i);
+        seq_printf(m, "  Bus: %03d, Device: %03d\n", curr->bus_num, curr->device_num);
+        seq_printf(m, "  VID:PID: 0x%04x:0x%04x\n", curr->vendor_id, curr->product_id);
+        seq_printf(m, "  Manufacturer: %s\n", curr->manufacturer);
+        seq_printf(m, "  Product: %s\n", curr->product);
+        seq_printf(m, "  Serial: %s\n", curr->serial);
+        seq_printf(m, "  Added: %lld.%09lu\n", (long long)curr->added_time.tv_sec, curr->added_time.tv_nsec);
+        if (!curr->is_connected)
+            seq_printf(m, "  Removed: %lld.%09lu\n", (long long)curr->removed_time.tv_sec, curr->removed_time.tv_nsec);
+        seq_printf(m, "  Status: %s\n\n", curr->is_connected ? "\033[32mConnected\033[0m" : "\033[31mDisconnected\033[0m");
+        curr = curr->next;
+        i++;
+    }
     return 0;
 }
 
@@ -232,7 +219,7 @@ static int usb_for_each_dev_callback(struct usb_device *device, void *ptr)
     {
         return err;
     }
-    add_device_to_array(device);
+    add_usb_device(device);
     return 0;
 }
 
@@ -260,27 +247,11 @@ static int init_procfs(void)
     return 0;
 }
 
-static int init_usb_devices_array(void)
-{
-    usb_devices_array = kmalloc_array(MAX_USB_DEVICES, sizeof(struct usb_device_info), GFP_KERNEL);
-    if (!usb_devices_array)
-    {
-        printk(KERN_ERR PROC_FILENAME ": Failed to allocate memory for USB devices array\n");
-        return -1;
-    }
-    memset(usb_devices_array, 0, MAX_USB_DEVICES * sizeof(struct usb_device_info));
-    device_count = 0;
-    return 0;
-}
-
 static int __init usb_activity_monitor_init(void)
 {
     printk(KERN_INFO PROC_FILENAME ": Init usb_activity_monitor module\n");
     int err;
     err = init_procfs();
-    if (err)
-        goto out;
-    err = init_usb_devices_array();
     if (err)
         goto out;
     err = list_usb_devices();
@@ -294,11 +265,6 @@ out:
         proc_remove(proc_file);
         proc_file = NULL;
     }
-    if (usb_devices_array)
-    {
-        kfree(usb_devices_array);
-        usb_devices_array = NULL;
-    }
     return err;
 }
 
@@ -309,11 +275,6 @@ static void __exit usb_activity_monitor_exit(void)
     {
         proc_remove(proc_file);
         proc_file = NULL;
-    }
-    if (usb_devices_array)
-    {
-        kfree(usb_devices_array);
-        usb_devices_array = NULL;
     }
     usb_unregister_notify(&usb_notifier_block);
 }
